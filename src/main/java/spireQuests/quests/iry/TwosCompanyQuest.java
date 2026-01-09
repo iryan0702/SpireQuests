61fd7eaf3325e247f40fc913a554fcb08ff53a44
iryan0702 <iryan0702@gmail.com>
2026-01-08 21:40:47 -0800

    fix: Simplify Two's Company Quest Trigger
    Move start of turn patch location to AbstractPlayer's applyStartOfTurnRelics. Move Two's Company Quest enemy count check on victory from patches to QuestTrigger condition.

--- src/main/java/spireQuests/quests/iry/TwosCompanyQuest.java
+++ src/main/java/spireQuests/quests/iry/TwosCompanyQuest.java
@@ -2,21 +2,23 @@ package spireQuests.quests.iry;
 
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
+import spireQuests.patches.QuestTriggers;
 import spireQuests.quests.AbstractQuest;
 import spireQuests.quests.QuestReward;
-import spireQuests.quests.Trigger;
+import spireQuests.quests.iry.patches.TwosCompanyPatches;
 import spireQuests.quests.iry.relics.BrokenHeartPendant;
 
 // TwosCompanyQuest:
-// Have 2 enemies alive on your final turn of combat for 3 combats
+// Have 2 enemies alive at the start of your final turn of combat for 3 combats
 // Rewards the Broken Heart Pendant
 public class TwosCompanyQuest extends AbstractQuest {
-    public static final Trigger<Void> TWOS_COMPANY = new Trigger<>();
 
     public TwosCompanyQuest() {
         super(QuestType.SHORT, QuestDifficulty.NORMAL);
 
-        new TriggerTracker<>(TWOS_COMPANY, 3).add(this);
+        new TriggerTracker<>(QuestTriggers.COMBAT_END, 3)
+                .triggerCondition(x -> TwosCompanyPatches.enemiesAliveThisTurn == 2)
+                .add(this);
 
         AbstractRelic rewardRelic = new BrokenHeartPendant();
         addReward(new QuestReward.RelicReward(rewardRelic));
